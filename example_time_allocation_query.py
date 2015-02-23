42181e264c8ca94c2050fb92f064bf3ab85994b1
Martin Norbury <mnorbury@lcogt.net>
2015-02-23 18:07:41 +0000

    Tidy up example

--- example_time_allocation_query.py
+++ example_time_allocation_query.py
@@ -1,38 +1,65 @@
+"""
+example_time_allocation_query.py - A simple example script demonstration a query requesting the current time
+used/available.
+
+Author:
+    Martin Norbury (mnorbury@lcogt.net)
+
+February 2015
+"""
 import urllib
-import httplib
 import json
 import getpass
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--host', default='scheduler-dev')
-parser.add_argument('-u', '--user', default='mnorbury@lcogt.net')
-parser.add_argument('-p', '--proposal', default='LCOSchedulerTest')
-parser.add_argument('-s', '--secure', action='store_true')
-args = parser.parse_args()
 
-protocol = 'https' if args.secure else 'http'
-url='%s://%s/observe/proposal/%s/time_summary/' % (protocol,
-                                                   args.host,
-                                                   args.proposal)
+def _query_available_time(args):
+    url = '{0:s}://{1:s}/observe/proposal/{2:s}/time_summary/'.format('https' if args.secure else 'http',
+                                                                      args.host,
+                                                                      args.proposal)
 
-print 'Using url %s' % url
+    print 'Using url %s' % url
 
-credentials = {'username' : args.user,
-               'password' : getpass.getpass(),
-              }
+    credentials = {'username': args.user,
+                   'password': getpass.getpass(), }
 
-connection = urllib.urlopen(url, data=urllib.urlencode(credentials))
-response = connection.read()
-try:
-    response = json.loads(response)
-except ValueError:
-    pass
-print response
-print
-print
+    connection = urllib.urlopen(url, data=urllib.urlencode(credentials))
+    response = connection.read()
+    connection.close()
 
-print '2m time :-'
-print
-for key, value in response[args.proposal]['2m0'].iteritems():
-    print '{0:<30} {1}'.format(key, value)
+    return response
+
+
+def _parse_response(response):
+    try:
+        response = json.loads(response)
+    except ValueError:
+        pass
+
+    return response
+
+
+def _print_telescope_class_summary(telescope_class, time_for_proposal):
+    print '%s time :-' % telescope_class
+    print
+    for key, value in time_for_proposal[telescope_class].iteritems():
+        print '{0:<30} {1}'.format(key, value)
+
+    print
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='scheduler-dev')
+    parser.add_argument('-u', '--user', default='mnorbury@lcogt.net')
+    parser.add_argument('-p', '--proposal', default='LCOSchedulerTest')
+    parser.add_argument('-s', '--secure', action='store_true')
+    configuration_arguments = parser.parse_args()
+
+    raw_response = _query_available_time(configuration_arguments)
+
+    time_allocation = _parse_response(raw_response)
+
+    for telescope_class in time_allocation[configuration_arguments.proposal].keys():
+        _print_telescope_class_summary(telescope_class, time_allocation[configuration_arguments.proposal])
+
